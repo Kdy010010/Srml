f1c685491f64fed3243cc0beefa0d1ed8844ffd0
Kdy010010 <137839917+Kdy010010@users.noreply.github.com>
2023-10-19 17:44:04 +0900

    Create Srml-main.py

---
+++ Srml-main.py
@@ -0,0 +1,54 @@
+import xml.etree.ElementTree as ET
+
+class SRMLInterpreter:
+    def __init__(self):
+        self.server_functions = {}
+        self.client_html = ""
+
+    def _interpret_server(self, server):
+        for func in server:
+            name = func.tag
+            self.server_functions[name] = func.text
+
+    def _interpret_element(self, element):
+        if element.tag == "input":
+            return f'<input type="{element.attrib["type"]}" name="{element.attrib["name"]}" placeholder="{element.attrib.get("placeholder", "")}" />\n'
+        elif element.tag == "textarea":
+            return f'<textarea name="{element.attrib["name"]}">{element.text or ""}</textarea>\n'
+        elif element.tag == "button":
+            return f'<button onclick="{element.attrib["onclick"]}">{element.text or ""}</button>\n'
+        elif element.tag == "div":
+            content = ''.join(self._interpret_element(child) for child in element)
+            return f'<div id="{element.attrib["id"]}">{content}</div>\n'
+        elif element.tag == "a":
+            return f'<a href="{element.attrib["href"]}">{element.text or ""}</a>\n'
+        elif element.tag == "img":
+            return f'<img src="{element.attrib["src"]}" alt="{element.attrib.get("alt", "")}" />\n'
+        elif element.tag == "p":
+            content = ''.join(self._interpret_element(child) for child in element)
+            return f'<p>{content}</p>\n'
+        # ... Continue for other HTML tags as needed
+
+    def _interpret_client(self, client):
+        for element in client:
+            self.client_html += self._interpret_element(element)
+
+    def interpret_from_string(self, srml_code):
+        root = ET.fromstring(srml_code)
+        for child in root:
+            if child.tag == "server":
+                self._interpret_server(child)
+            elif child.tag == "client":
+                self._interpret_client(child)
+
+        return self.client_html
+
+    def interpret_from_file(self, file_path):
+        with open(file_path, 'r', encoding='utf-8') as f:
+            content = f.read()
+            return self.interpret_from_string(content)
+
+# Usage example:
+interpreter = SRMLInterpreter()
+html_output = interpreter.interpret_from_file('path_to_file.srml')
+print(html_output)
